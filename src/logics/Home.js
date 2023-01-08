10241dfcdbf31c05e095d75db2617e3ca29d4d49
Kenobichek <lesechklesia@gmail.com>
2023-01-08 15:44:02 +0200

    Feature PopUp
    The popup was created using js.
    
    This popup contains a contact form.
    
    There are also several ways to hide the modal window:
    1) hiding the popup when clicking on the background
    2) by clicking on the cross

--- src/logics/Home.js
+++ src/logics/Home.js
@@ -1,4 +1,9 @@
 window.addEventListener("load", (event) => {
+    const popupBg = document.querySelector('.popup__bg'); 
+    const popup = document.querySelector('.popup'); 
+    const openPopupButtons = document.querySelectorAll('.open-popup');
+    const closePopupButton = document.querySelector('.close-popup');
+
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
@@ -8,4 +13,25 @@ window.addEventListener("load", (event) => {
             });
         });
     });
+      
+    openPopupButtons.forEach((button) => { 
+        button.addEventListener('click', (e) => { 
+            e.preventDefault(); 
+            popupBg.classList.add('active');
+            popup.classList.add('active');
+        })
+    });
+
+    closePopupButton.addEventListener('click',() => { 
+       popupBg.classList.remove('active'); 
+       popup.classList.remove('active');
+    });
+
+    document.addEventListener('click', (e) => {
+        if(e.target === popupBg) { 
+            popupBg.classList.remove('active');
+            popup.classList.remove('active'); 
+        }
+    });
+    
 });
